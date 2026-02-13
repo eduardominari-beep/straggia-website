d0d5b784f3d0df2037959006066b969fd847172b
Eduardo Minari <eduardo.minari@gmail.com>
2026-02-13 19:47:44 -0300

    Add GTM to landing pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 // app/layout.tsx
 import type { Metadata } from "next"
+import Script from "next/script"
 import { Inter, Poppins } from "next/font/google"
 import "./globals.css"
 import { getSEOMetadata } from "@/lib/seo"
 import SeoJsonLd from "@/components/SeoJsonLd"
 
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.straggia.com"
+const GTM_ID = "GTM-PQTK2WTQ"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 const poppins = Poppins({ subsets: ["latin"], weight: ["400","500","600","700"], variable: "--font-poppins" })
@@ -17,8 +19,8 @@ export const metadata: Metadata = {
   }),
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },                 // .ico multi-res
-      { url: "/icon.png", type: "image/png", sizes: "48x48" } // PNG para o Google
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png", sizes: "48x48" }
     ],
     apple: [{ url: "/apple-icon.png", sizes: "180x180" }],
     shortcut: ["/favicon.ico"],
@@ -29,7 +31,36 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${poppins.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <SeoJsonLd siteUrl={SITE_URL} sameAs={["https://www.linkedin.com/company/straggia"]} />
         {children}
       </body>
